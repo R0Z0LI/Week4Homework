71d884f88613ffba65267833e8d09c975c522b1c
Rohringer Zoltan <rohringer.zoltan@gmail.com>
2023-03-21 11:10:36 +0100

    feat: adding new element

--- dist/script.js
+++ dist/script.js
@@ -1,2 +1,17 @@
 "use strict";
-console.log("Hello there");
+const addButton = document.getElementById("add-btn");
+const weightDiv = document.getElementById("weight");
+const dateDiv = document.getElementById("date");
+let historyData = [];
+//today's date
+addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener("click", addWeight);
+function addWeight() {
+    const firstElem = {
+        weight: +weightDiv.value,
+        date: dateDiv.value,
+    };
+    historyData.push(firstElem);
+    for (let i = 0; i < historyData.length; i++) {
+        console.log(historyData[i].date);
+    }
+}
--- dist/script.ts
+++ dist/script.ts
@@ -1,1 +1,25 @@
-console.log("Hello there");
+const addButton = document.getElementById("add-btn");
+const weightDiv = document.getElementById("weight")! as HTMLInputElement;
+const dateDiv = document.getElementById("date")! as HTMLInputElement;
+
+type weightElement = {
+  weight: number;
+  date: string;
+};
+
+let historyData: weightElement[] = [];
+
+//today's date
+
+addButton?.addEventListener("click", addWeight);
+
+function addWeight() {
+  const firstElem: weightElement = {
+    weight: +weightDiv.value,
+    date: dateDiv.value,
+  };
+  historyData.push(firstElem);
+  for (let i = 0; i < historyData.length; i++) {
+    console.log(historyData[i].date);
+  }
+}
