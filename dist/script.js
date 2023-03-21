dd3b18e941d345e4feb9dcdd49a197468d0c40f6
Rohringer Zoltan <rohringer.zoltan@gmail.com>
2023-03-21 15:06:41 +0100

    fix: history date format

--- dist/script.js
+++ dist/script.js
@@ -3,6 +3,10 @@ const addButton = document.getElementById("add-btn");
 const weightDiv = document.getElementById("weight");
 const dateDiv = document.getElementById("date");
 const historyContainer = document.getElementById("history__container");
+const date = new Date();
+const thisYear = date.getFullYear.toString;
+const thisMonth = date.getMonth.toString;
+const today = date.getDay.toString;
 let historyData = [];
 addButton === null || addButton === void 0 ? void 0 : addButton.addEventListener("click", addWeight);
 function addWeight() {
@@ -13,18 +17,38 @@ function addWeight() {
     };
     historyData.push(firstElem);
     sort();
-    historyData.forEach((element) => {
-        console.log(element);
-    });
     let i = 0;
     historyData.forEach((element) => {
+        const year = element.date.slice(0, 4);
+        const month = element.date.slice(5, 7);
+        const day = element.date.slice(8, 10);
+        const hour = element.date.slice(11, 13);
+        const min = element.date.slice(14, 16);
+        const chosenDate = new Date(+year, +month, +day, +hour, +min);
         const newDivElement = document.createElement("div");
         newDivElement.setAttribute("id", "history__row_" + i);
         historyContainer.appendChild(newDivElement);
         const newWeight = document.createElement("span");
-        newWeight.innerHTML = element.weight.toString();
+        newWeight.innerHTML = element.weight.toString() + " kg    ";
         const newDate = document.createElement("span");
-        newDate.innerHTML = element.date;
+        console.log(chosenDate.getUTCDate());
+        console.log(date.getUTCDate());
+        if (chosenDate.getFullYear() !== date.getFullYear()) {
+            let string = day + " " + month + " " + year + " at " + hour + ":" + min;
+            newDate.innerHTML = string;
+        }
+        else if (chosenDate.getUTCDate() === date.getUTCDate()) {
+            let string = "today at " + hour + ":" + min;
+            newDate.innerHTML = string;
+        }
+        else if (chosenDate.getUTCDate() === date.getUTCDate() - 1) {
+            let string = "yesterday at " + hour + ":" + min;
+            newDate.innerHTML = string;
+        }
+        else {
+            let string = day + " " + month + " at " + hour + ":" + min;
+            newDate.innerHTML = string;
+        }
         newDivElement.appendChild(newWeight);
         newDivElement.appendChild(newDate);
         i++;
