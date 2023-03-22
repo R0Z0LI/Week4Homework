16d0189e5cb08ca6832a98e193e579346888bc22
Rohringer Zoltan <rohringer.zoltan@gmail.com>
2023-03-22 08:23:40 +0100

    fix: maximum first 10 data

--- dist/script.js
+++ dist/script.js
@@ -19,38 +19,40 @@ function addWeight() {
     sort();
     let i = 0;
     historyData.forEach((element) => {
-        const year = element.date.slice(0, 4);
-        const month = element.date.slice(5, 7);
-        const day = element.date.slice(8, 10);
-        const hour = element.date.slice(11, 13);
-        const min = element.date.slice(14, 16);
-        const chosenDate = new Date(+year, +month, +day, +hour, +min);
-        const newDivElement = document.createElement("div");
-        newDivElement.setAttribute("id", "history__row_" + i);
-        historyContainer.appendChild(newDivElement);
-        const newWeight = document.createElement("span");
-        newWeight.innerHTML = element.weight.toString() + " kg    ";
-        const newDate = document.createElement("span");
-        console.log(chosenDate.getUTCDate());
-        console.log(date.getUTCDate());
-        if (chosenDate.getFullYear() !== date.getFullYear()) {
-            let string = day + " " + month + " " + year + " at " + hour + ":" + min;
-            newDate.innerHTML = string;
+        if (i < 10) {
+            const year = element.date.slice(0, 4);
+            const month = element.date.slice(5, 7);
+            const day = element.date.slice(8, 10);
+            const hour = element.date.slice(11, 13);
+            const min = element.date.slice(14, 16);
+            const chosenDate = new Date(+year, +month, +day, +hour, +min);
+            const newDivElement = document.createElement("div");
+            newDivElement.setAttribute("id", "history__row_" + i);
+            historyContainer.appendChild(newDivElement);
+            const newWeight = document.createElement("span");
+            newWeight.innerHTML = element.weight.toString() + " kg    ";
+            const newDate = document.createElement("span");
+            console.log(chosenDate.getUTCDate());
+            console.log(date.getUTCDate());
+            if (chosenDate.getFullYear() !== date.getFullYear()) {
+                let string = day + " " + month + " " + year + " at " + hour + ":" + min;
+                newDate.innerHTML = string;
+            }
+            else if (chosenDate.getUTCDate() === date.getUTCDate()) {
+                let string = "today at " + hour + ":" + min;
+                newDate.innerHTML = string;
+            }
+            else if (chosenDate.getUTCDate() === date.getUTCDate() - 1) {
+                let string = "yesterday at " + hour + ":" + min;
+                newDate.innerHTML = string;
+            }
+            else {
+                let string = day + " " + month + " at " + hour + ":" + min;
+                newDate.innerHTML = string;
+            }
+            newDivElement.appendChild(newWeight);
+            newDivElement.appendChild(newDate);
         }
-        else if (chosenDate.getUTCDate() === date.getUTCDate()) {
-            let string = "today at " + hour + ":" + min;
-            newDate.innerHTML = string;
-        }
-        else if (chosenDate.getUTCDate() === date.getUTCDate() - 1) {
-            let string = "yesterday at " + hour + ":" + min;
-            newDate.innerHTML = string;
-        }
-        else {
-            let string = day + " " + month + " at " + hour + ":" + min;
-            newDate.innerHTML = string;
-        }
-        newDivElement.appendChild(newWeight);
-        newDivElement.appendChild(newDate);
         i++;
     });
 }
